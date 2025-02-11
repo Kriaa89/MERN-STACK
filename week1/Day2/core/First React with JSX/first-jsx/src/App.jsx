0e23d6b806d0b662cd049327703f711b4805ce5e
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-11 17:58:23 +0100

    Initialize React project with Vite setup and basic components

---
+++ week1/Day2/core/First React with JSX/first-jsx/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App(){
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li> Run a marthon</li>
+            <li>Feed The dogs</li>
+        </ul>
+    </>
+  );
+}
+export default App;
