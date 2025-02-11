851ddc8259317ea3fb315241fcbaf47f3bd05cc5
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-11 18:57:21 +0100

    Refactor App component structure and add new layout components

---
+++ week1/Day3/Core/React Blocks/react-blocks/src/components/Maincontent.jsx
@@ -0,0 +1,15 @@
+import Advertisement from './Advertisement';
+import Subcontent from './Subcontent';
+
+const Maincontent = () => {
+    return(
+        <div className="main">
+            
+            <Subcontent/>
+            <Subcontent/>
+            <Subcontent/>
+            <Advertisement/>
+        </div>
+    )
+} 
+export default Maincontent;
