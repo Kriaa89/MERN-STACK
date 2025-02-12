aac5a01468a6855db6ac9ecc8cf300090d625d87
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-12 17:58:17 +0100

    Format App component for consistency and readability

--- week1/Day3/practice/Prop It Up/prop-it-up/src/App.jsx
+++ week1/Day3/practice/Prop It Up/prop-it-up/src/App.jsx
@@ -2,27 +2,27 @@ import './App.css'
 import './index.css'
 import PersonCard from './components/Person'
 function App() {
-  return (
-      <div className='card'>
-          <PersonCard
-              firstName={"John"}
-              lastName={"Doe"}
-              age={45}
-              hairColor={"Black"}
-          />
-          <PersonCard
-              firstName={"John"}
-              lastName={"Doe"}
-              age={45}
-              hairColor={"Black"}
-          />
-          <PersonCard
-              firstName={"John"}
-              lastName={"Doe"}
-              age={45}
-              hairColor={"Black"}
-          />
-      </div>
+    return (
+        <div className='card'>
+            <PersonCard
+                firstName={"John"}
+                lastName={"Doe"}
+                age={45}
+                hairColor={"Black"}
+            />
+            <PersonCard
+                firstName={"John"}
+                lastName={"Doe"}
+                age={45}
+                hairColor={"Black"}
+            />
+            <PersonCard
+                firstName={"John"}
+                lastName={"Doe"}
+                age={45}
+                hairColor={"Black"}
+            />
+        </div>
     );
 }
 export default App;
