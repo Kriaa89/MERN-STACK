cdf38fa8aaf87c9cc1297eb0a622b7ee8f6b94ee
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-14 18:26:52 +0100

    Refactor date formatting utilities to include relative time and advanced format functions

--- week1/Day2/practice/isodate-formatter/index.js
+++ week1/Day2/practice/isodate-formatter/index.js
@@ -5,8 +5,5 @@ const testDate = [
     "2021-04-21T09:40:26+0000"
 ]
 testDate.forEach(Date => {
-    console.log(dayjs(Date).format("MM/DD/YYYY"));
-    console.log(Date);
-    console.log(dayjs(Date).format("MMMM D, YYYY"));
-    console.log(dayjs(Date).format("dddd"))
+    console.log("longs date")
 })
--- week1/Day2/practice/isodate-formatter/utils/formatDate.js
+++ week1/Day2/practice/isodate-formatter/utils/formatDate.js
@@ -1,2 +1,20 @@
 import dayjs from "dayjs";
-const dayjs = require("dayjs");
+import relativeTime from "dayjs/plugin/releativeTime";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+
+dayjs.extend(relativeTime);
+dayjs.extend(advancedFormat);
+
+// this 
+const formatRelativeTime = (date) => {
+    return dayjs(date).fromNow();
+};
+const formatAdancedFormat = (date) => {
+    return dayjs(date).format("dddd, MMMM D, YYYY")
+};
+
+const formatShort = (date) => {
+    return dayjs(date).format("MM/DD/YYYY")
+};
+
+export { formatRelativeTime, formatAdancedFormat, formatShort}
