cab22aa06f12c9b549bfddc300c7fae23aad270d
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-14 18:12:57 +0100

    Refactor date formatting utilities to use CommonJS syntax and remove unused plugins

--- week1/Day2/practice/isodate-formatter/utils/formatDate.js
+++ week1/Day2/practice/isodate-formatter/utils/formatDate.js
@@ -1,20 +1,2 @@
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/releativeTime";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-
-dayjs.extend(relativeTime);
-dayjs.extend(advancedFormat);
-
-// this 
-const formatRelativeTime = (date) => {
-    return dayjs(date).fromNow();
-};
-const formatAdancedFormat = (date) => {
-    return dayjs(date).format("dddd, MMMM D, YYYY")
-};
-
-const formatShort = (date) => {
-    return dayjs(date).format("MM/DD/YYYY")
-};
-
-export { formatRelativeTime, formatAdancedFormat, formatShort };
+const dayjs = require("dayjs");
