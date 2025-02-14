e7b76ab00851c470c8a400a92afab2fbd7cc775e
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-14 18:21:36 +0100

    Add test cases for date formatting using dayjs

--- week1/Day2/practice/isodate-formatter/index.js
+++ week1/Day2/practice/isodate-formatter/index.js
@@ -1,1 +1,12 @@
 import dayjs from "dayjs";
+const testDate = [
+    "2007-04-21T09:40:26+0000",
+    "2019-04-21T09:40:26+0000",
+    "2021-04-21T09:40:26+0000"
+]
+testDate.forEach(Date => {
+    console.log(dayjs(Date).format("MM/DD/YYYY"));
+    console.log(Date);
+    console.log(dayjs(Date).format("MMMM D, YYYY"));
+    console.log(dayjs(Date).format("dddd"))
+})
