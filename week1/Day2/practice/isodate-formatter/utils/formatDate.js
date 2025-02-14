ffd80f2d8f98a1e134264d9b06a3a92729ae6d03
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-14 17:56:34 +0100

    Add dayjs plugins for relative time and advanced formatting

--- week1/Day2/practice/isodate-formatter/utils/formatDate.js
+++ week1/Day2/practice/isodate-formatter/utils/formatDate.js
@@ -2,5 +2,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/releativeTime";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 
+dayjs.extend(relativeTime);
+dayjs.extend(advancedFormat);
 
-
+const 
