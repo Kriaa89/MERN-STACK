f961ad793c63579ab909a8e1e0e6e1351d6f8495
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-16 13:15:15 +0100

    Implement Todo rendering with completion toggle in TodoList component

--- week1/Day5/Core/Todo List/BoxGenerator/src/components/TodoList.jsx
+++ week1/Day5/Core/Todo List/BoxGenerator/src/components/TodoList.jsx
@@ -2,7 +2,11 @@ const TodoList = (props) => {
     const { todos, deleteTodo, toggleTodo} = props;
     return (
         <ul>
-            
+            {todos.map(todo => (
+                <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through': 'none'}}>
+                    <input type="checkbox" checked={todo.completed}  onChange={() => toggleTodo(todo.id)}/>
+                </li>
+            ))}
         </ul>
     )
 }
