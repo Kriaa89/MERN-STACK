28bbb20d33e8f6c5b58c0bef086c7206a2df8f8a
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-16 15:37:03 +0100

    Refactor App component to integrate TodoForm and TodoList components

--- week1/Day5/Core/Todo List/BoxGenerator/src/components/TodoList.jsx
+++ week1/Day5/Core/Todo List/BoxGenerator/src/components/TodoList.jsx
@@ -5,8 +5,11 @@ const TodoList = (props) => {
             {todos.map(todo => (
                 <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through': 'none'}}>
                     <input type="checkbox" checked={todo.completed}  onChange={() => toggleTodo(todo.id)}/>
+                    {todo.task}
+                    <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                 </li>
             ))}
         </ul>
-    )
+    );
 }
+export default TodoList;
