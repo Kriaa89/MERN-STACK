5d96a65fd0b143a0fe3cede4eb423b633b37a15b
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-20 16:10:23 +0100

    Add routing and search functionality with SearchForm component

--- week2/Day2/Practice/luke-apiwalker/src/App.jsx
+++ week2/Day2/Practice/luke-apiwalker/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchForm from "./components/SearchForm";
+import Character from "./views/Character";
+import Planet from "./views/Planet";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <SearchForm/>
+        <Routes>
+          <Route path="/people/:id" element={<Character/>}/>
+          <Route path="/planetes/:id" element={<Planet/>}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- week2/Day2/Practice/luke-apiwalker/src/components/SearchForm.jsx
+++ week2/Day2/Practice/luke-apiwalker/src/components/SearchForm.jsx
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SearchForm = () => {
-    const [name, setName] = useState("")
+    const [resource, setResourse ] = useState('people');
+    const [id , setId] = useState('')
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate();
+        navigate("/:resource/:id", { resource, id});
     };
     return (
-
-    )
-}
+        <form onSubmit={handleSubmit}>
+            <select value={resource} onChange={(e) => setResourse(e.target.value)}>
+                <option value="people">People</option>
+                <option value="planests">Planets</option>
+            </select>
+            <input type="number" value={id} onChange={(e) => setId(e.target.value)}/>
+            <button type="submit">Search</button>
+        </form>
+    );
+};
+export default SearchForm;
