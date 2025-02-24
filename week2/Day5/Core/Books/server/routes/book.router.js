4439d96b0585075fbb19b2f452e3c52aada21c75
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-24 01:15:19 +0100

    Add book router with CRUD operations

---
+++ week2/Day5/Core/Books/server/routes/book.router.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { createBook, getAllBooks, getOneBook, updateBook, deleteBook} from '../controllers/book.controller.js';
+
+
+
+const router = Router();
+
+router.post('/books', createBook);
+router.get('/books', getAllBooks);
+router.get('/books/: id', getOneBook);
+router.patch('/books/ :id', updateBook);
+router.delete('/books/:id', deleteBook);
+
+
+export default router;
+
