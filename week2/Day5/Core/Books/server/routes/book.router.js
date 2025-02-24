a03927771f5521ea91db9d82e0d748897219dbfd
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-24 01:56:11 +0100

    Fix routing syntax for book retrieval and update endpoints

--- week2/Day5/Core/Books/server/routes/book.router.js
+++ week2/Day5/Core/Books/server/routes/book.router.js
@@ -7,8 +7,8 @@ const router = Router();
 
 router.post('/books', createBook);
 router.get('/books', getAllBooks);
-router.get('/books/: id', getOneBook);
-router.patch('/books/ :id', updateBook);
+router.get('/books/:id', getOneBook);
+router.patch('/books/:id', updateBook);
 router.delete('/books/:id', deleteBook);
 
 
