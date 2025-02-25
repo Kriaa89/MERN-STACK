5d9515394f068d2e3f50e391f6d0c745b7895e3a
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-25 18:48:55 +0100

    Implement BookForm and BookList components for managing book entries

--- week2/Day5/Core/MERN_Books/client/src/views/BookList.jsx
+++ week2/Day5/Core/MERN_Books/client/src/views/BookList.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react"; // import UseState and UseEffect to use hooks in the function
+import { useEffect, useState } from "react"; // import UseState and UseEffect to use hooks in the function
 import {  useNavigate } from "react-router-dom"; // import Link to use it in the return
 import axios from "axios"; // import axios to make requests to the server
 
 
 function BookForm() {
-    const [title, setTitle] = useState("");
-    const [author, setAuthor] = useState("");
-    const [pages, setPages] = useState("");
-    const [isAvailable, setIsAvailable] = useState(true);
-    const navigate = useNavigate();
+    const [books, setBooks] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post("http://localhost:8000/api/books", {
-            title,
-            author,
-            pages : parseInt(pages), // we use parseInt to convert the string to an integer
-            isAvailable
-        })
-        .then(() => navigate('/'))
-        .catch(console.log(err));
-    };
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/book")
+            .then(res => setBooks(res.data))
+            .catch(err => console.log(err));
+    }, []);
     return (
-        <form onSubmit={handleSubmit}>
-            
+        <div className="container">
+            <table className="book-table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Pages</th>
+                        <th>Available</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map(book => (
+                        <tr key={book._id}>
+                            <td>{book.title}</td>
+                            <td>{book.title}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
-        </form>
+        </div>
     )
 }
