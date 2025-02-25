d2cc1b9cfe25e970a37f4a8938e4a77d93dcf7ec
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-25 19:48:13 +0100

    Add Header component with navigation links

--- week2/Day5/Core/MERN_Books/client/src/components/Header.jsx
+++ week2/Day5/Core/MERN_Books/client/src/components/Header.jsx
@@ -1,1 +1,13 @@
 import { Link } from "react-router-dom";
+function Header() {
+    return (
+        <div className="header">
+            <h1>Favorite Book</h1>
+            <div className="nav-links">
+                <Link></Link>
+                <Link></Link>
+            </div>
+            
+        </div>
+    )
+}
