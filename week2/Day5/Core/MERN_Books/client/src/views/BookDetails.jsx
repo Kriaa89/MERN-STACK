b70d1e0763165ec53a202fbca7fc4cedcdb0a345
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-25 19:43:20 +0100

    Implement BookDetails component with API integration for fetching and deleting book data

--- week2/Day5/Core/MERN_Books/client/src/views/BookDetails.jsx
+++ week2/Day5/Core/MERN_Books/client/src/views/BookDetails.jsx
@@ -1,4 +1,32 @@
+import axios from "axios";
 import { useEffect, useState } from "react"; // import UseState and UseEffect to use hooks in the function
 import { useParams, useNavigate } from "react-router-dom";
 
-function 
+function BookDetails() {
+    const [book, setBook] = useState(null)
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/book/${id}`)
+            .then(res => setBook(res.data))
+            .catch(err => console.log(err));
+    }, [id]);
+
+    const handleBorrow = () => {
+        axios.delete(`http://localhost:8000/api/book/${id}`)
+            .then(() => navigate('/'))
+            .catch(err => console.log(err));
+    };
+    return (
+        <div className="detail-container">
+            <h2>{book.title}</h2>
+            <div className="detail-container">
+                <p><strong>Author:</strong></p>
+                <p><strong>Pages:</strong></p>
+                <p><strong>Status:</strong></p>
+
+            </div>
+        </div>
+    )
+}
