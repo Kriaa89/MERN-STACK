caa48ef302021a36418b65f0d67496dcb55818c0
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-26 11:24:29 +0100

    Implement updateBook function with state management and API call setup

--- week3/Core/Day1/MERNing with Books III/mern-books/client/src/views/UpdateBook.jsx
+++ week3/Core/Day1/MERNing with Books III/mern-books/client/src/views/UpdateBook.jsx
@@ -1,3 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams} from 'react-router-dom';
 import axios from 'axios'; 
+
+function updateBook() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [book, setBook] = useState({
+        title: "",
+        author: "",
+        pages: "",
+        isAvailable: true
+    });
+    const [errors , setErrors] = useState({});
+    useEffect(() => {
+        axios.get(``)
+    })
+}
