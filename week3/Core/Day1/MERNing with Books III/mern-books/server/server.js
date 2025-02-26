b69d34b1e037598979c0d005f076f54635146779
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-26 11:59:05 +0100

    Refactor server setup to include database connection and routing

--- week3/Core/Day1/MERNing with Books III/mern-books/server/server.js
+++ week3/Core/Day1/MERNing with Books III/mern-books/server/server.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-*
 
-
+import dbConnect from './config/mongoose.config.js';
+import router from './routes/book.routes.js';
 
 const app = express();
 app.use(express.json(), cors());
-
-
 dotenv.config();
 
 const PORT = process.env.PORT;
+
 app.use('/api', router);
 dbConnect();
-app.listen(PORT, () =>
-    console.log(`Listening on port: ${PORT}`)
+
+app.listen(PORT, () => 
+    console.log(`Server is running on port ${PORT}`)
 );
