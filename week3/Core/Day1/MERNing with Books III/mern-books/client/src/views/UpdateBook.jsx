db96eb08761779285a8c00fc771473af7a97f5c8
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-26 11:30:39 +0100

    Implement book update functionality with error handling and navigation

--- week3/Core/Day1/MERNing with Books III/mern-books/client/src/views/UpdateBook.jsx
+++ week3/Core/Day1/MERNing with Books III/mern-books/client/src/views/UpdateBook.jsx
@@ -14,5 +14,16 @@ function updateBook() {
     const [errors , setErrors] = useState({});
     useEffect(() => {
         axios.get(`http://localhost:8000/api/book/${id}`)
-    })
+            .then(res => setBook(res.data))
+            .catch(err => console.log(err));
+    }, [id]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.put(`http://localhost:8000/api/book/${id}`, book)
+            .then(res => navigate('/'))
+            .catch(err => setErrors(err.resonse.data.errors));
+    };
+    return (
+        
+    )
 }
