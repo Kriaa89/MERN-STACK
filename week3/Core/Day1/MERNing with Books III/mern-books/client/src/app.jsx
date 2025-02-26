5e5e76ca717437ad813b85610ccef778b74298b0
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-02-26 13:10:07 +0100

    Refactor App component to use React Router for navigation and remove unused code

--- week3/Core/Day1/MERNing with Books III/mern-books/client/src/app.jsx
+++ week3/Core/Day1/MERNing with Books III/mern-books/client/src/app.jsx
@@ -1,43 +1,24 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import BookList from './views/BookList'
+import BookDetails from './views/BookDetails'
+import BookForm from './views/BookForm'
+import UpdateBook  from './views/UpdateBook'
 
-export function App() {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<BookList/>}></Route>
+        <Route path='/books/new' element={<BookForm/>}></Route>
+        <Route path='/books/:id' element={<BookDetails/>}></Route>
+        <Route path='/books/:id/edit' element={<UpdateBook/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+
+export default App;
+
